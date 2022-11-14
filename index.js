76b40f0d70be47be002e09215a94a3bbaf6e1b68
Mhamad6000 <mhamadothman6000@gmail.com>
2022-11-14 23:44:39 +0300

    done but i used diffrent way

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const value = fetch("https://anapioficeandfire.com/api/books");
+  return value;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks()
+    .then((res) => res.json())
+    .then((json) => {
+      renderBooks(json);
+    });
 });
